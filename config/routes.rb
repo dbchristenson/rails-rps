6e858e5668d0ce34aeaf090a72a9413709b1ba55
DB Christenson <dbeauchristenson@gmail.com>
2024-04-23 15:45:09 +0000

    routes defined

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  get("/", { :controller => "pages", :action => "index"})
+  get("/rock", { :controller => "pages", :action => "rock"})
+  get("/paper", { :controller => "pages", :action => "paper"})
+  get("/scissors", { :controller => "pages", :action => "scissors"})
 end
